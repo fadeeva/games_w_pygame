ca60c3fc1986eb2c4a388bd55ab67a3da9a90c27
Katya Fadeeva <katfadmail@gmail.com>
2024-12-12 17:27:38 +0200

    ludo king icon

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ display_height = 700
 
 black  = (3, 3, 15)
 
-ludo_king_icon = '../ai_psd_source_files/ludo_king/ludo_king_shadow.png'
+ludo_king_icon = 'img/ludo_king_icon.png'
 
 def game_loop():
     running = True
@@ -13,7 +13,7 @@ def game_loop():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+        pygame.display.update()
             
 if __name__ == '__main__':
     pygame.init()
@@ -22,10 +22,10 @@ if __name__ == '__main__':
     pygame.display.set_caption('GAME W/ PYGAME')
     clock = pygame.time.Clock()
 
-    ludo_king = pygame.image.load(ludo_king_icon).convert()
+    ludo_king = pygame.image.load(ludo_king_icon).convert_alpha()
     
     game_display.fill(black)
-    game_display.blit(ludo_king, (100, 182))
+    game_display.blit(ludo_king, (365, 236))
     
     game_loop()
     
