d3acea3e3c10228cbe653a97d7ae4a4eba111aff
Katya Fadeeva <katfadmail@gmail.com>
2025-01-01 19:34:56 +0200

    added elements on screen

--- src/games/ludo_king/main.py
+++ src/games/ludo_king/main.py
@@ -1,17 +1,38 @@
 import pygame
 
+
+COLORS = {
+    'beige': (201, 184, 167),
+    'light beige': (229, 219, 207),
+    'dark beige': (160, 146, 135),
+    'black': (35, 31, 32)
+}
+
 SETTINGS = {
-        'board': 'games/ludo_king/img/board.svg',
-        'bg_clr': (201, 184, 167),
-        'caption': 'Ludo King'
+        'bg_clr': COLORS['beige'],
+        'caption': 'Ludo King',
+}
+
+ATTR = {
+    'board': 'games/ludo_king/img/board.svg',
+    'circle': 'games/ludo_king/img/circle.svg',
+    'dice': 'games/ludo_king/img/dice.svg',
 }
 
 
 def draw_board(game_display):
-    board_img = SETTINGS['board']
-    
-    board = pygame.image.load(board_img)
-
     pygame.display.set_caption(SETTINGS['caption'])
     game_display.fill(SETTINGS['bg_clr'])
+    
+    board = pygame.image.load(ATTR['board'])
     game_display.blit(board, (218, 60))
+    
+    circle = pygame.image.load(ATTR['circle'])
+    game_display.blit(circle, (63, 125))
+    game_display.blit(circle, (841, 483))
+    
+    dice = pygame.image.load(ATTR['dice'])
+    game_display.blit(dice, (80, 141))
+    game_display.blit(dice, (858, 497))
+    
+    
